b42a334dec07ddaa622b03468cd8ff31dc60e411
webguy83 <curtisyacboski@gmail.com>
2019-02-09 23:02:45 -0800

    changes to git build

---
+++ client/build/precache-manifest.f95e7fb7b5636bd10da073ef78ffd446.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cce506cabaaa8c2704e9",
+    "url": "/static/js/main.cce506ca.chunk.js"
+  },
+  {
+    "revision": "33aa07d607d3c0025827",
+    "url": "/static/js/1.33aa07d6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9feacf2839fd63a29f66df975cf69704",
+    "url": "/index.html"
+  }
+];
